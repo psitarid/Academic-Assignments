12a5e392422a4e1052743c26d97993388419794e
psitarid <132507840+psitarid@users.noreply.github.com>
2025-06-19 15:54:46 +0300

    Add files via upload

---
+++ Μικροεπεξεργαστές και Περιφερειακά/Lab1/Objects/c_final.d
@@ -0,0 +1,6 @@
+./objects/c_final.o: C_final.c \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  drivers\drivers\uart.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\string.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdlib.h
---
+++ Μικροεπεξεργαστές και Περιφερειακά/Lab1/Objects/queue.d
@@ -0,0 +1,3 @@
+./objects/queue.o: drivers\drivers\queue.c drivers\drivers\queue.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdlib.h

d5c3993753da3cc5bc77a7eaacd81d9c10684e35
psitarid <132507840+psitarid@users.noreply.github.com>
2025-06-19 15:57:48 +0300

    Add files via upload

---
+++ Μικροεπεξεργαστές και Περιφερειακά/Lab2/Objects/c_lab2.d
@@ -0,0 +1,16 @@
+./objects/c_lab2.o: C_lab2.c \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  drivers\gpio.h drivers\platform.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\STM32F4xx.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  D:\University\Microprocessors\ Lab\keil\ packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
+  drivers\uart.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\string.h \
+  D:\University\Microprocessors\ Lab\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  drivers\leds.h drivers\queue.h
